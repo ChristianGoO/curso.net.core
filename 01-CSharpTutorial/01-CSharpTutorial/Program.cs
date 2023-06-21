71ae3a66972b621c7e8336b15759325f4ef2405f
cristiangoo <chris.gon.06@gmail.com>
2023-06-20 18:05:46 -0600

    Program CSharpTutorial

---
+++ 01-CSharpTutorial/01-CSharpTutorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_CSharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello world";
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
