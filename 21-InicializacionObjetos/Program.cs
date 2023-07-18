d8c2e62f8a33c5aad0251963dd04eac09ecce385
ChristianGoO <chris.gon.06@gmail.com>
2023-07-17 18:10:22 -0600

    Ejercicios

---
+++ 21-InicializacionObjetos/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_InicializacionObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var std0 = new Estidiante()
+            {
+                StudentID = 1,
+                StudentName = "Bill",
+                Age = 20,
+                Address = "New York"
+            };
+
+            var std1 = new Estidiante();
+            std1.StudentID = 1;
+            std1.StudentName = "Bill";
+            std1.Age = 20;
+            std1.Address = "New York";
+
+            var std2 = new Estidiante() { StudentID = 1, StudentName = "maria" };
+            var std3 = new Estidiante() { StudentID = 2, StudentName = "juan" };
+            var std4 = new Estidiante() { StudentID = 3, StudentName = "pedro" };
+            var std5 = new Estidiante() { StudentID = 3, StudentName = "alberto" };
+            var std6 = new Estidiante() { StudentID = 5, StudentName = "fernando" };
+
+            var studentList = new List<Estidiante>() {
+                                                        std2,
+                                                        std3,
+                                                        std4,
+                                                        std5,
+                                                        std6
+                                                     };
+            IList<Estidiante> studentList1 = new List<Estidiante>() {
+                                                                        new Estidiante() { StudentID = 1, StudentName = "maria" },
+                                                                        new Estidiante() { StudentID = 2, StudentName = "juan" },
+                                                                        new Estidiante() { StudentID = 3, StudentName = "pedro" },
+                                                                        new Estidiante() { StudentID = 3, StudentName = "alberto" },
+                                                                        new Estidiante() { StudentID = 5, StudentName = "fernando" },
+                                                                        null
+                                                                    };
+
+        }
+    }
+
+
+    public class Estidiante
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ 22-CovarianzaContravarianza/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_CovarianzaContravarianza
+{
+    internal class Program
+    {
+
+        public delegate Small CovarDelegado(big mc);
+
+        static void Main(string[] args)
+        {
+            
+            Small small = new Small();
+            small.id = 1;
+            small.nombre = "juan";
+            //small.tamaño = 2.30f
+
+            big big = new big();
+            big.id = 2;
+            big.nombre = "maria";
+            big.descripcion = "persona grande";
+            //big.tamaño = 2.30f
+
+            bigger bigger = new bigger();
+            bigger.id = 3;
+            bigger.nombre = "pedro";
+            bigger.descripcion = "persona extragrande";
+            bigger.tamaño = 2.30f;
+
+            //CREACION DE OBJETOS Y ASIGNACION DE VARIABLES
+            Small small1 = new Small();
+            Small small2 = new big();
+            Small small3 = new bigger();
+            big big1 = new bigger();
+            //big big2 = new Small();
+
+            CovarDelegado del1 = Method1;
+            Small sm1 = del1(new big());
+
+            CovarDelegado del2 = Method2;
+            Small sm2 = del2(new big());
+
+            CovarDelegado del3 = Method3;
+            Small sm3 = del3(new big());
+
+            CovarDelegado del4 = Method2;
+            //big sm4 = del4(new big());
+
+            CovarDelegado del5 = Method4;
+            Small sm5 = del5(new big());
+
+
+            CovarDelegado del = Method1;
+            del += Method2;
+            del += Method3;
+            del += Method4;
+
+            Small small5 = del(new big());
+
+
+            CovarDelegado del6 = Method5;
+            Small sm6 = del6(new big());
+
+        }
+
+
+        public static big Method1(big bg)
+        {
+            Console.WriteLine("Method1");
+
+            return new big();
+        }
+        public static Small Method2(big bg)
+        {
+            Console.WriteLine("Method2");
+
+            return new Small();
+        }
+
+        public static bigger Method3(big bg)
+        {
+            Console.WriteLine("Method3x|");
+
+            return new bigger();
+        }
+
+        public static Small Method4(Small sml)
+        {
+            Console.WriteLine("Method2");
+
+            return new Small();
+        }
+
+        static big Method5(Small sml)
+        {
+            Console.WriteLine("Method3");
+
+            return new big();
+        }
+
+    }
+
+
+    public class Small
+    {
+        public int id { get; set; }
+
+        public string nombre { get; set; }
+    }
+
+    public class big : Small
+    {
+        public string descripcion { get; set; }
+    
+    }
+
+    public class bigger : big
+    {
+        public float tamaño { get; set; }
+    }
+}
